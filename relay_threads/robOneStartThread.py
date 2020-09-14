6f3e427b5dbb41bd4c6b2586f32c2c3a5ce6ce79
JayWang <2643267669@qq.com>
2020-09-14 16:53:45 +0800

    初始框架

---
+++ relay_threads/robOneStartThread.py
@@ -0,0 +1,27 @@
+# -*- encoding: UTF-8 -*-
+"""
+1号机器人等待出发的线程
+"""
+from threading import Thread
+from naoqi import ALProxy
+
+
+class RobOneStartThread(Thread):
+    def __init__(self, robot_conf, start_move_queue):
+        super(self.__class__, self).__init__()
+
+        self.__robot_conf = robot_conf
+        self.__start_move_queue = start_move_queue
+
+        self.__touch = ALProxy("ALTouch",
+                               self.__robot_conf['basic_param']['ip'],
+                               self.__robot_conf['basic_param']['port'])
+
+    def run(self):
+        while True:
+            status = self.__touch.getStatus()
+            for e in status:
+                if e[0] == 'Head' and e[1]:
+                    self.__start_move_queue.put('start')
+                    break
+
