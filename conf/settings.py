bfe466c66f0423cf55680a652b12f6b48e15a477
JayWang <2643267669@qq.com>
2020-09-17 18:39:47 +0800

    更新运动参数

--- conf/settings.py
+++ conf/settings.py
@@ -5,7 +5,7 @@
 
 robot1_conf = {                                         # 1号机器人参数
     'basic_param': {                                    # 基础参数
-        'ip': '192.168.43.250',
+        'ip': '172.20.10.8',
         'port': 9559,
     },
     'motion_param': {                                   # 运动参数
@@ -14,39 +14,39 @@ robot1_conf = {                                         # 1号机器人参数
             'y': 0,
             'theta': 0,
             'config': [
-                ["MaxStepFrequency", 0.2],
-                ["MaxStepTheta", 0.0005],
-                ["MaxStepX", 0.02],
-                ["MaxStepY", 0.101],
-                ["StepHeight", 0.005],
+                ["MaxStepFrequency", 0.8],
+                ["MaxStepTheta", 0.4],
+                ["MaxStepX", 0.05],
+                ["MaxStepY", 0.2],
+                ["StepHeight", 0.01],
                 ["TorsoWx", 0],
                 ["TorsoWy", 0]
             ]
         },
         'left': {                                       # 左转
-            'x': 0,
+            'x': 0.8,
             'y': 0,
-            'theta': 0.5,
+            'theta': 0.05,
             'config': [
-                ["MaxStepFrequency", 0.2],
-                ["MaxStepTheta", 0.0005],
-                ["MaxStepX", 0.02],
-                ["MaxStepY", 0.101],
-                ["StepHeight", 0.005],
+                ["MaxStepFrequency", 0.8],
+                ["MaxStepTheta", 0.4],
+                ["MaxStepX", 0.05],
+                ["MaxStepY", 0.2],
+                ["StepHeight", 0.01],
                 ["TorsoWx", 0],
                 ["TorsoWy", 0]
             ]
         },
         'right': {                                      # 右转
-            'x': 0,
+            'x': 0.8,
             'y': 0,
-            'theta': -0.5,
+            'theta': -0.1,
             'config': [
-                ["MaxStepFrequency", 0.2],
-                ["MaxStepTheta", 0.0005],
-                ["MaxStepX", 0.02],
-                ["MaxStepY", 0.101],
-                ["StepHeight", 0.005],
+                ["MaxStepFrequency", 0.8],
+                ["MaxStepTheta", 0.4],
+                ["MaxStepX", 0.05],
+                ["MaxStepY", 0.2],
+                ["StepHeight", 0.01],
                 ["TorsoWx", 0],
                 ["TorsoWy", 0]
             ]
--- relay_main.py
+++ relay_main.py
@@ -36,20 +36,20 @@ def main():
     r1vision = VisionThread(robot1_conf, r1_start_vision_queue, r1_vision_move_queue)
     r1vision.start()
 
-    # 2号机器人等待出发线程
-    r2start = RobTwoStartThread(robot2_conf, r2_start_move_queue, r2_start_vision_queue)
-    r2start.start()
-    # 2号机器人运动线程
-    r2motion = RobTwoMoveThread(robot2_conf, r2_start_move_queue, r2_vision_move_queue)
-    r2motion.start()
-    # 2号机器人视觉线程
-    r2vision = VisionThread(robot2_conf, r2_start_vision_queue, r2_vision_move_queue)
-    r2vision.start()
+    # # 2号机器人等待出发线程
+    # r2start = RobTwoStartThread(robot2_conf, r2_start_move_queue, r2_start_vision_queue)
+    # r2start.start()
+    # # 2号机器人运动线程
+    # r2motion = RobTwoMoveThread(robot2_conf, r2_start_move_queue, r2_vision_move_queue)
+    # r2motion.start()
+    # # 2号机器人视觉线程
+    # r2vision = VisionThread(robot2_conf, r2_start_vision_queue, r2_vision_move_queue)
+    # r2vision.start()
 
     # wait for join
-    r2vision.join()
-    r2motion.join()
-    r2start.join()
+    # r2vision.join()
+    # r2motion.join()
+    # r2start.join()
     r1vision.join()
     r1motion.join()
     r1start.join()
